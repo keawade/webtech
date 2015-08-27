f87f837a74b8a922d9d6bac1a4b133333c643d6e
Keith Wade <keawade@gmail.com>
2015-08-27 21:44:04 +0000

    Hello world implemented with Express and Handlebars.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var http = require('http');
+var hbs = require('hbs');
+
+var app = express();
+var server = http.createServer(app);
+
+app.set('view engine', 'hbs');
+app.engine('html', hbs.__express);
+
+app.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'Hello!'
+    });
+});
+
+server.listen(process.env.PORT || 8000, process.env.IP || "0.0.0.0", function(){
+    var addr = server.address();
+    console.log("[webtech] listening at ", addr.address + ":" + addr.port);
+});
