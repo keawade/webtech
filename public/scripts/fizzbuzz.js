a9bc1877fcfc741b392274d637fcbf7afee8851b
Keith Wade <keawade@gmail.com>
2015-09-02 00:15:01 -0500

    Working fizzbuzz button
    Clicking the button changes the button text appropriately.

--- public/scripts/fizzbuzz.js
+++ public/scripts/fizzbuzz.js
@@ -1,62 +1,49 @@
-/*
 var App = React.createClass({
     getInitialState:function(){
         return {
-            num: 0
+            value: 1,
+            text: 1
         };
     },
     fizzbuzz:function(){
-        this.setState({num: this.state.num + 1});
-    },
-    fizzbuzz5:function(){
-        for (var i = 0; i < 5; i++) {
-            this.setState({num: this.state.num + 1});
+        if ( ( ( this.state.value % 3 ) == 0 ) && ( ( this.state.value % 5 ) == 0 ) ) {
+            this.setState({value: this.state.value + 1});
+            this.setState({text: "Fizzbuzz"});
+        } else if ( ( this.state.value % 3 ) == 0 ) {
+            this.setState({value: this.state.value + 1});
+            this.setState({text: "Fizz"});
+        } else if ( ( this.state.value % 5 ) == 0 ) {
+            this.setState({value: this.state.value + 1});
+            this.setState({text: "Buzz"});
+        } else {
+            this.setState({value: this.state.value + 1});
+            this.setState({text: this.state.value});
         }
-    },
-    fizzbuzz10:function(){
-        for (var i = 0; i < 10; i++) {
-            this.setState({num: this.state.num + 1});
-        }
-    },
-    fizzbuzz100:function(){
-        for (var i = 0; i < 100; i++) {
-            this.setState({num: this.state.num + 1});
-        }
+        console.log("Value: " + this.state.value);
+        console.log("Text: " + this.state.text);
     },
     render:function(){
         return (
             <div>
                 <h1>Fizzbuzz</h1>
-                <div className="btn-group" role="group" aria-label="...">
-                    <button type="button" className="btn btn-default" placeholder={this.state.name} onClick={this.fizzbuzz}>1</button>
-                    <button type="button" className="btn btn-default" placeholder={this.state.name} onClick={this.fizzbuzz5}>5</button>
-                    <button type="button" className="btn btn-default" placeholder={this.state.name} onClick={this.fizzbuzz10}>10</button>
-                    <button type="button" className="btn btn-default" placeholder={this.state.name} onClick={this.fizzbuzz100}>100</button>
-                </div>
-                <ul id="fizzbuzzer" className="fa-ul">
-                    <li><i className="fa-li fa fa-circle-o"></i>{this.state.num}</li>
-                    <li><i className="fa-li fa fa-minus fa-rotate-90 fa-fw"></i>Fizz</li>
-                    <li><i className="fa-li fa fa-plus fa-fw"></i>Fizzbuzz</li>
-                    <li><i className="fa-li fa fa-minus fa-fw"></i>Buzz</li>
-                </ul>
+                <button type="button" className="btn btn-default col-md-1" onClick={this.fizzbuzz}>{this.state.text}</button>
             </div>
         );
     }
 });
 
 React.render(<App />, document.getElementById('fizzbuzz'));
-*/
 
 /*
 Ex2: Fizzbuzz.js
 Create a file called “fizzbuzz.js”, which does the following:
-1. Logs the numbers from 1 to 100 to the console, but for multiples of three 
+1. Logs the numbers from 1 to 100 to the console, but for multiples of three
 (3), log “Fizz” instead of the number, and for the multiples of five (5) print
 “Buzz”. For numbers which are multiples of both three and five, print “FizzBuzz”.
 */
 
-function fizzbuzz() {
-    for( var i = 1 ; i < 16 ; i++ ) {
+function fizzbuzzConsole() {
+    for( var i = 1 ; i < 101 ; i++ ) {
         if ( ( ( i % 3 ) == 0 ) && ( ( i % 5 ) == 0 ) ) {
             console.log("FizzBuzz");
         } else if ( ( i % 3 ) == 0 ) {
@@ -68,5 +55,13 @@ function fizzbuzz() {
         }
     }
 }
+//fizzbuzzConsole();
 
-fizzbuzz();
+/*
+<ul id="fizzbuzzer" className="fa-ul">
+    <li><i className="fa-li fa fa-circle-o"></i>{this.state.num}</li>
+    <li><i className="fa-li fa fa-minus fa-rotate-90 fa-fw"></i>Fizz</li>
+    <li><i className="fa-li fa fa-plus fa-fw"></i>Fizzbuzz</li>
+    <li><i className="fa-li fa fa-minus fa-fw"></i>Buzz</li>
+</ul>
+*/
